409003f73f5a1c3ee2d28222f3939a95f050385e
Projects <antonywilson229@gmail.com>
2025-06-29 15:55:10 +0800

    notifications

--- savewiser/lib/main.dart
+++ savewiser/lib/main.dart
@@ -1,15 +1,57 @@
-// File: lib/main.dart
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'setup_page.dart';
 import 'main_nav.dart';
 
-void main() {
+// Plugin instance (make sure it's initialized in main.dart)
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+    FlutterLocalNotificationsPlugin();
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Request notification permission for Android 13+ (POST_NOTIFICATIONS)
+  await _requestNotificationPermission();
+
+  // Create the notification channel for Android 8.0+
+  const AndroidNotificationChannel channel = AndroidNotificationChannel(
+    'test_channel_id', // Channel ID
+    'Test Notifications', // Channel Name
+    description: 'This channel is used for test notifications',
+    importance: Importance.max,
+    playSound: true,
+  );
+
+  // Initialize plugin settings for Android
+  const AndroidInitializationSettings androidInitialization =
+      AndroidInitializationSettings('@mipmap/ic_launcher');
+
+  final InitializationSettings initializationSettings = InitializationSettings(
+    android: androidInitialization,
+  );
+
+  await flutterLocalNotificationsPlugin.initialize(initializationSettings);
+
+  // Create the notification channel
+  await flutterLocalNotificationsPlugin
+      .resolvePlatformSpecificImplementation<
+        AndroidFlutterLocalNotificationsPlugin
+      >()
+      ?.createNotificationChannel(channel);
+
   runApp(SaveWiserApp());
+}
+
+Future<bool> _requestNotificationPermission() async {
+  PermissionStatus status = await Permission.notification.request();
+  return status.isGranted;
 }
 
 class SaveWiserApp extends StatelessWidget {
   const SaveWiserApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -23,6 +65,7 @@ class SaveWiserApp extends StatelessWidget {
 
 class InitialScreenDecider extends StatefulWidget {
   const InitialScreenDecider({super.key});
+
   @override
   State<InitialScreenDecider> createState() => _InitialScreenDeciderState();
 }
@@ -53,147 +96,3 @@ class _InitialScreenDeciderState extends State<InitialScreenDecider> {
     return isSetupDone! ? MainNavigation() : SetupPage();
   }
 }
-
-// import 'package:flutter/material.dart';
-// import 'package:savewiser/main_nav.dart';
-
-// void main() {
-//   runApp(const MyApp());
-// }
-
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return const MaterialApp(
-//       debugShowCheckedModeBanner: false,
-//       home: MainNavigation(),
-//     );
-//   }
-// }
-
-// import 'package:flutter/material.dart';
-// import 'package:savewiser/pages/home.dart';
-// import 'second_page.dart';
-
-// void main() {
-//   runApp(const MyApp());
-// }
-
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-
-//   // This widget is the root of your application.
-//   @override
-
-//   Widget build(BuildContext context) {
-//     return const MaterialApp(
-//       // title: 'Flutter Demo',
-//       // theme: ThemeData(
-//       //   // This is the theme of your application.
-//       //   //
-//       //   // TRY THIS: Try running your application with "flutter run". You'll see
-//       //   // the application has a purple toolbar. Then, without quitting the app,
-//       //   // try changing the seedColor in the colorScheme below to Colors.green
-//       //   // and then invoke "hot reload" (save your changes or press the "hot
-//       //   // reload" button in a Flutter-supported IDE, or press "r" if you used
-//       //   // the command line to start the app).
-//       //   //
-//       //   // Notice that the counter didn't reset back to zero; the application
-//       //   // state is not lost during the reload. To reset the state, use hot
-//       //   // restart instead.
-//       //   //
-//       //   // This works for code too, not just values: Most code changes can be
-//       //   // tested with just a hot reload.
-//       //   colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-//       // ),
-//       // home: const MyHomePage(title: 'Flutter Demo Home Page'),
-//       debugShowCheckedModeBanner: false,
-//       home: HomePage(),
-//     );
-//   }
-// }
-
-// class MyHomePage extends StatefulWidget {
-//   const MyHomePage({super.key, required this.title});
-
-//   // This widget is the home page of your application. It is stateful, meaning
-//   // that it has a State object (defined below) that contains fields that affect
-//   // how it looks.
-
-//   // This class is the configuration for the state. It holds the values (in this
-//   // case the title) provided by the parent (in this case the App widget) and
-//   // used by the build method of the State. Fields in a Widget subclass are
-//   // always marked "final".
-
-//   final String title;
-
-//   @override
-//   State<MyHomePage> createState() => _MyHomePageState();
-// }
-
-// class _MyHomePageState extends State<MyHomePage> {
-//   int _counter = 0;
-
-//   void _incrementCounter() {
-//     setState(() {
-//       // This call to setState tells the Flutter framework that something has
-//       // changed in this State, which causes it to rerun the build method below
-//       // so that the display can reflect the updated values. If we changed
-//       // _counter without calling setState(), then the build method would not be
-//       // called again, and so nothing would appear to happen.
-//       _counter += 3;
-//     });
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     // This method is rerun every time setState is called, for instance as done
-//     // by the _incrementCounter method above.
-//     //
-//     // The Flutter framework has been optimized to make rerunning build methods
-//     // fast, so that you can just rebuild anything that needs updating rather
-//     // than having to individually change instances of widgets.
-//     return Scaffold(
-//       appBar: AppBar(
-//         // TRY THIS: Try changing the color here to a specific color (to
-//         // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-//         // change color while the other colors stay the same.
-//         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-//         // Here we take the value from the MyHomePage object that was created by
-//         // the App.build method, and use it to set our appbar title.
-//         title: Text(widget.title),
-//       ),
-//       body: Center(
-//         // Center is a layout widget. It takes a single child and positions it
-//         // in the middle of the parent.
-//         child: Column(
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: <Widget>[
-//             const Text('You have pushed the button this many times:'),
-//             Text(
-//               '$_counter',
-//               style: Theme.of(context).textTheme.headlineMedium,
-//             ),
-//             const SizedBox(height: 20),
-//             ElevatedButton(
-//               onPressed: () {
-//                 Navigator.push(
-//                   context,
-//                   MaterialPageRoute(builder: (context) => const SecondPage()),
-//                 );
-//               },
-//               child: const Text('Go to Second Page'),
-//             ),
-//           ],
-//         ),
-//       ),
-//       floatingActionButton: FloatingActionButton(
-//         onPressed: _incrementCounter,
-//         tooltip: 'Increment',
-//         child: const Icon(Icons.add),
-//       ), // This trailing comma makes auto-formatting nicer for build methods.
-//     );
-//   }
-// }
--- savewiser/lib/pages/future_statistics.dart
+++ savewiser/lib/pages/future_statistics.dart
@@ -1,10 +1,53 @@
+// import 'package:flutter/material.dart';
+
+// class FutureStatisticsPage extends StatelessWidget {
+//   const FutureStatisticsPage({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Center(child: Text('Future Statistic Page'));
+//   }
+// }
+
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+// Plugin instance (make sure it's initialized in main.dart)
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+    FlutterLocalNotificationsPlugin();
 
 class FutureStatisticsPage extends StatelessWidget {
   const FutureStatisticsPage({super.key});
 
+  // Method to show test notification
+  void _showTestNotification() async {
+    const AndroidNotificationDetails androidDetails =
+        AndroidNotificationDetails(
+          'test_channel_id', // Channel ID
+          'Test Notifications', // Channel name
+          importance: Importance.max,
+          priority: Priority.high,
+        );
+
+    const NotificationDetails platformDetails = NotificationDetails(
+      android: androidDetails,
+    );
+
+    await flutterLocalNotificationsPlugin.show(
+      0, // Notification ID
+      'Test Notification',
+      'This is a test notification from FutureStatisticsPage!',
+      platformDetails,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('Future Statistic Page'));
+    return Center(
+      child: ElevatedButton(
+        onPressed: _showTestNotification,
+        child: const Text('Test Notification'),
+      ),
+    );
   }
 }
