63eb3dfbeaa8a54dd55e6b222659d7f2e06c149b
Bryan Gilbert <bryangilbert1411@gmail.com>
2025-07-04 21:56:53 +0800

    abc

--- savewiser/lib/main.dart
+++ savewiser/lib/main.dart
@@ -90,7 +90,7 @@ class _InitialScreenDeciderState extends State<InitialScreenDecider> {
     final prefs = await SharedPreferences.getInstance();
     final done = prefs.getBool('isSetupDone') ?? false;
     setState(() {
-      isSetupDone = false; // ✅ Use the value from SharedPreferences
+      isSetupDone = done; // ✅ Use the value from SharedPreferences
     });
   }
 
