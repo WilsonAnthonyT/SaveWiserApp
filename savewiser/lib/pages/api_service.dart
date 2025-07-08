69c7f423333af57d9f56ed6b5e11468bd7425d99
Bryan Gilbert <bryangilbert1411@gmail.com>
2025-07-09 04:18:05 +0800

    Add prompt

--- savewiser/lib/pages/api_service.dart
+++ savewiser/lib/pages/api_service.dart
@@ -5,7 +5,10 @@ class ApiService {
   static const _baseUrl = 'https://openrouter.ai/api/v1/chat/completions';
   static const _apiKey = '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';
 
-  Future<http.Response> fetchAdvice() {
+  Future<http.Response> fetchAdvice(double moneySaved, String goalDate, double pace, String goal) {
+    final int currentYear = DateTime.now().year;
+    final int currentDay = DateTime.now().day;
+    final int currentMonth = DateTime.now().month;
     final uri = Uri.parse(_baseUrl);
     final headers = {
       'Content-Type': 'application/json',
@@ -16,7 +19,7 @@ class ApiService {
       'messages': [
         {
           'role': 'user',
-          'content': 'Currently I have saved Rp 20.000.000, I am saving at the rate of Rp 20.000 per day, my goal is to save Rp 100.000.000 by 30th November 2025, today is 30th June 2025. Give me 20 words or less detailed actionable advice for me to reach my goal. Be to the point no need to add any excess characters like \'*\' or \'(20 words)\'. Use English.'
+          'content': 'Currently I have saved Rp $moneySaved, I am saving at the rate of Rp $pace per month, my goal is to save Rp $goal by $goalDate, today is $currentDay $currentMonth $currentYear. Give me 20 words or less detailed actionable advice for me to reach my goal. Be to the point no need to add any excess characters like \'*\' or \'(20 words)\'. Use English.'
         }
       ],
     });
